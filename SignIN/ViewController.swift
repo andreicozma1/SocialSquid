a5fe8430c2310ea77e5747d9021f5f1020de20ed
Andrei Cozma <andreig992@gmail.com>
2020-06-03 19:03:19 -0500

    Added callback code for printing out which button was clicked

--- SignIN/ViewController.swift
+++ SignIN/ViewController.swift
@@ -15,6 +15,33 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    // Define callbacks for clicking each one of our UI Elements
+    @IBAction func google(_ sender: UIButton) {
+        printClickedElement(btnTitle: (sender.titleLabel?.text)!)
+    }
+    @IBAction func facebook(_ sender: UIButton) {
+        printClickedElement(btnTitle: (sender.titleLabel?.text)!)
+    }
+    @IBAction func email(_ sender: UIButton) {
+        printClickedElement(btnTitle: (sender.titleLabel?.text)!)
+    }
+    
+    // Helper function to print out a log
+    func printClickedElement(btnTitle: String){
+        let dateString = Date().toString(dateFormat: "yyyy-MM-dd HH:mm:ss")
+        
+        print("Clicked on " + btnTitle + " at " + dateString)
+    }
+}
 
+extension Date{
+    // here dateFormat stands for the label of the argument we supply
+    func toString(dateFormat format: String) -> String{
+        // We then use the format keyword to reference our argument
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        // Since we are extending the Date object, we can reference 'self' when using the formatter
+        return formatter.string(from: self)
+    }
 }
 
